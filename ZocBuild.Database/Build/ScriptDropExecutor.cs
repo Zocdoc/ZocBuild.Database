97037774c0a93835e75f63d562604771807257f3
Kyle Peter Fritz <kyle.fritz@zocdoc.com>
2013-01-30 15:42:01 -0500

    Execute scripts in a transaction so that failures can be rolled back.

--- ZocBuild.Database/Build/ScriptDropExecutor.cs
+++ ZocBuild.Database/Build/ScriptDropExecutor.cs
@@ -9,19 +9,21 @@ namespace ZocBuild.Database.Build
 {
     class ScriptDropExecutor : IScriptExecutor
     {
-        public ScriptDropExecutor(SqlConnection connection)
+        public ScriptDropExecutor(SqlConnection connection, SqlTransaction transaction)
         {
             Connection = connection;
+            Transaction = transaction;
         }
 
         private SqlConnection Connection { get; set; }
+        private SqlTransaction Transaction { get; set; }
 
         public async Task ExecuteAsync(ScriptFile script, BuildItem.BuildActionType action)
         {
             if(action == BuildItem.BuildActionType.Drop || action == BuildItem.BuildActionType.DropAndCreate)
             {
                 string cmdText = string.Format("DROP {0} [{1}].[{2}]", script.ScriptObject.ObjectType.ToString(), script.ScriptObject.SchemaName, script.ScriptObject.ObjectName);
-                var cmd = new SqlCommand(cmdText, Connection);
+                var cmd = new SqlCommand(cmdText, Connection, Transaction);
                 await cmd.ExecuteNonQueryAsync();
             }
         }
