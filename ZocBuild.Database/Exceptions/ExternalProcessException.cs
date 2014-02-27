07d4393244332bce4bf7d35e41d9e836ca2f050b
Kyle Peter Fritz <kyle.fritz@zocdoc.com>
2014-02-27 11:05:01 -0500

    Create a contract for running external processes.  To be used by the DvcsScriptRepositories.

---
+++ ZocBuild.Database/Exceptions/ExternalProcessException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZocBuild.Database.Exceptions
+{
+    public class ExternalProcessException : Exception
+    {
+        public ExternalProcessException(string executablePath, int exitCode, string output, string error)
+            : base(string.Format("The executable located at '{0}' exited with code {1}. The error text follows:{2}", executablePath, exitCode, Environment.NewLine + (error ?? string.Empty)))
+        {
+            ExecutablePath = executablePath;
+            ExitCode = exitCode;
+            StandardOutput = output;
+            StandardError = error;
+        }
+
+        public string ExecutablePath { get; private set; }
+        public int ExitCode { get; private set; }
+        public string StandardOutput { get; private set; }
+        public string StandardError { get; private set; }
+    }
+}
---
+++ ZocBuild.Database/ExternalProcess.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZocBuild.Database.Exceptions;
+
+namespace ZocBuild.Database
+{
+    /// <summary>
+    /// Defines the behavior of an object that can execute external processes.
+    /// </summary>
+    public interface IExternalProcess
+    {
+        /// <summary>
+        /// Executes the external process and returns the results from standard out.
+        /// </summary>
+        /// <exception cref="FileNotFoundException">Thrown if the process executable doesn't exist or is inaccessible.</exception>
+        /// <exception cref="ExternalProcessException">Thrown if a non-zero exit code is returned from the process.</exception>
+        /// <param name="arguments">The arguments to supply to the process.</param>
+        /// <param name="workingDirectory">The path to the working directory in which the process should execute.</param>
+        /// <returns>The text on standard out.</returns>
+        Task<string> ExecuteAsync(string arguments, string workingDirectory);
+    }
+
+    /// <summary>
+    /// An object that can start an external process and read its output.
+    /// </summary>
+    public class ExternalProcess : IExternalProcess
+    {
+        private readonly string _executablePath;
+
+        /// <summary>
+        /// Constructs an external process object that can invoke the executable at the given path.
+        /// </summary>
+        /// <param name="executablePath"></param>
+        public ExternalProcess(string executablePath)
+        {
+            _executablePath = executablePath;
+        }
+
+        /// <summary>
+        /// Executes the external process and returns the results from standard out.
+        /// </summary>
+        /// <exception cref="FileNotFoundException">Thrown if the process executable doesn't exist or is inaccessible.</exception>
+        /// <exception cref="ExternalProcessException">Thrown if a non-zero exit code is returned from the process.</exception>
+        /// <param name="arguments">The arguments to supply to the process.</param>
+        /// <param name="workingDirectory">The path to the working directory in which the process should execute.</param>
+        /// <returns>The text on standard out.</returns>
+        public async Task<string> ExecuteAsync(string arguments, string workingDirectory)
+        {
+            string outputContent;
+            string errorContent;
+            using (var p = CreateProcess(arguments, workingDirectory))
+            {
+                p.Start();
+                outputContent = await p.StandardOutput.ReadToEndAsync();
+                errorContent = await p.StandardError.ReadToEndAsync();
+                p.WaitForExit();
+
+                if (p.ExitCode != 0)
+                {
+                    throw new ExternalProcessException(_executablePath, p.ExitCode, outputContent, errorContent);
+                }
+            }
+            return outputContent;
+        }
+
+        private Process CreateProcess(string arguments, string workingDirectory)
+        {
+            var p = new Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.WorkingDirectory = workingDirectory;
+            p.StartInfo.FileName = _executablePath;
+            p.StartInfo.Arguments = arguments;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            return p;
+        }
+    }
+}
